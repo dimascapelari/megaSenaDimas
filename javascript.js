9ab47051454b2cbe2bbc8c3e60f2d4810471ac7c
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-14 09:44:41 -0300

    subindoProjeto

---
+++ javascript.js
@@ -0,0 +1,34 @@
+let btn = document.getElementById('btn')
+let resultado1 = document.getElementById('resultado1')
+let resultado2 = document.getElementById('resultado2')
+let resultado3 = document.getElementById('resultado3')
+let resultado4 = document.getElementById('resultado4')
+let resultado5 = document.getElementById('resultado5')
+let resultado6 = document.getElementById('resultado6')
+
+function sorte(a, b) {
+    return Math.floor(Math.random() * (b - a + 1)) + a
+}
+
+function print() {
+    resultado1.innerHTML = `${sorte(1, 10)}`
+    resultado2.innerHTML = `${sorte(11, 20)}`
+    resultado3.innerHTML = `${sorte(21, 30)}`
+    resultado4.innerHTML = `${sorte(31, 40)}`
+    resultado5.innerHTML = `${sorte(41, 50)}`
+    resultado6.innerHTML = `${sorte(51, 60)}`
+}
+
+btn.addEventListener('click', print)
+
+
+/*
+function print() {
+    resultado1.innerHTML = `${sorte(1, 10)}`
+    resultado2.innerHTML = `Sorteado: ${sorte(11, 20)}`
+    resultado3.innerHTML = `Sorteado: ${sorte(21, 30)}`
+    resultado4.innerHTML = `Sorteado: ${sorte(31, 40)}`
+    resultado5.innerHTML = `Sorteado: ${sorte(41, 50)}`
+    resultado6.innerHTML = `Sorteado: ${sorte(51, 60)}`
+}
+*/
